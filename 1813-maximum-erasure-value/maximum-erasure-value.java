e8b1762ed495ce5a4c7011d8c5125e3a8304b034
Harsh Wardhan <harshkashyap1221@gmail.com>
2026-08-13 15:50:29 +0530

    Time: 98 ms (5.17%) | Memory: 91.8 MB (87.25%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -7,12 +7,7 @@ class Solution {
         int maxScore = 0;
 
         for(int i = 0; i < n; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }
-            else{
-                map.put(nums[i], 1);
-            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
 
             sum += nums[i];
 
