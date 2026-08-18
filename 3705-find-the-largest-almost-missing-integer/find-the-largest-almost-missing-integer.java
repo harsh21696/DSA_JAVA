6822f9c030e8725861f6c594396e9387a0170e93
Harsh Wardhan <harshkashyap1221@gmail.com>
2026-08-18 22:20:24 +0530

    Time: 1 ms (99.30%) | Memory: 44.4 MB (88.07%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,49 +1,43 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        // This maps a number to the total number of size-k subarrays it appears in
-        Map<Integer, Integer> subarrayCount = new HashMap<>();
-        
-        // This map maintains the frequency of numbers in the current sliding window
-        Map<Integer, Integer> windowMap = new HashMap<>();
-        
-        // 1. Initialize the first window of size k
-        for (int i = 0; i < k; i++) {
-            windowMap.put(nums[i], windowMap.getOrDefault(nums[i], 0) + 1);
+        int n = nums.length;
+        int maxNum = -1;
+
+        for(int num : nums){
+            maxNum = Math.max(maxNum, num);
         }
         
-        // Record all unique numbers in this first window
-        for (int num : windowMap.keySet()) {
-            subarrayCount.put(num, subarrayCount.getOrDefault(num, 0) + 1);
+        int[] freq = new int[maxNum + 1];
+        for(int num : nums){
+            freq[num]++;
+        }
+   
+        if(k == n){
+            return maxNum;
         }
         
-        // 2. Push the window to the right by one element at a time
-        for (int i = k; i < nums.length; i++) {
-            // Add the new element entering the window from the right
-            windowMap.put(nums[i], windowMap.getOrDefault(nums[i], 0) + 1);
-            
-            // Remove the old element leaving the window from the left
-            int leftElement = nums[i - k];
-            windowMap.put(leftElement, windowMap.get(leftElement) - 1);
-            
-            // If the frequency drops to 0, completely remove it from the window map
-            if (windowMap.get(leftElement) == 0) {
-                windowMap.remove(leftElement);
+        if(k == 1){
+            int maxMiss = -1;
+
+            for(int i = 0; i <= maxNum; i++){
+                if(freq[i] == 1){
+                    maxMiss= Math.max(maxMiss, i);
+                }
             }
-            
-            // Record all unique numbers present in this new current window
-            for (int num : windowMap.keySet()) {
-                subarrayCount.put(num, subarrayCount.getOrDefault(num, 0) + 1);
-            }
+
+            return maxMiss;
         }
         
-        // 3. Find the maximum number that appears in exactly one subarray
-        int maxAlmostMissing = -1;
-        for (Map.Entry<Integer, Integer> entry : subarrayCount.entrySet()) {
-            if (entry.getValue() == 1) {
-                maxAlmostMissing = Math.max(maxAlmostMissing, entry.getKey());
-            }
+        int maxMiss = -1;
+
+        if(freq[nums[0]] == 1){
+            maxMiss = Math.max(maxMiss, nums[0]);
+        }
+
+        if(freq[nums[n - 1]] == 1){
+            maxMiss = Math.max(maxMiss, nums[n - 1]);
         }
         
-        return maxAlmostMissing;
+        return maxMiss;
     }
 }
