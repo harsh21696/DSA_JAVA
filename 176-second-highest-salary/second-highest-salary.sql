fbd5cd5675a2ffa5af603f904387aaa8c279c687
Harsh Wardhan <harshkashyap1221@gmail.com>
2026-08-24 20:29:21 +0530

    Time: 323 ms (32.04%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-SELECT max(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary NOT IN(SELECT max(salary) FROM Employee)
+SELECT MAX(salary) AS SecondHighestSalary
+FROM (
+    SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    FROM Employee
+) AS ranked_salaries
+WHERE rnk = 2;
