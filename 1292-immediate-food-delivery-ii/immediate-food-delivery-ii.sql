b994684731095c7a5cbba590f227db0767a2b269
Harsh Wardhan <harshkashyap1221@gmail.com>
2026-08-25 23:10:13 +0530

    Time: 974 ms (11.67%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-SELECT ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
-FROM Delivery
-WHERE (customer_id, order_date) IN (SELECT customer_id, MIN(order_date)
-                                    FROM Delivery
-                                    GROUP BY customer_id);
+WITH FirstOrders AS (
+    SELECT 
+        customer_id,
+        order_date,
+        customer_pref_delivery_date,
+        LAG(order_date) OVER (PARTITION BY customer_id ORDER BY order_date) as prev_order_date
+    FROM Delivery
+)
+SELECT 
+    ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
+FROM FirstOrders
+WHERE prev_order_date IS NULL;
