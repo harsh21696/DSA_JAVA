4ce745b4145aacdaf718277541f31858c34396a1
Harsh Wardhan <harshkashyap1221@gmail.com>
2026-08-25 20:13:36 +0530

    Time: 746 ms (8.12%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+SELECT id
+FROM (SELECT id, recordDate, temperature,
+     LAG(temperature) OVER(ORDER BY recordDate) AS prev_temp,
+     LAG(recordDate) OVER(ORDER BY recordDate) AS prev_date
+     FROM weather) t
+WHERE temperature > prev_temp
+AND DATEDIFF(recordDate, prev_date) = 1;
+
+
+
+
